7295fa42dc98d6c941224bed29478af0ac24b683
Joyce Tawfik <joyce.tawfik@student.guc.edu.eg>
2017-06-13 06:02:14 -0700

    Add files via upload

--- Arduino.ino
+++ Arduino.ino
@@ -40,12 +40,85 @@ currentSpeed = string.toInt();
 else
 currentSpeed = string.toInt()+256;
 }
-analogWrite(9, currentSpeed);
-digitalWrite(8,HIGH);
-digitalWrite(10,LOW);
-analogWrite(5, currentSpeed);
-digitalWrite(6,HIGH);
-digitalWrite(7,LOW);
+//Backward
+ if(string == "B")
+  { digitalWrite(7,HIGH);
+   digitalWrite(8,HIGH);
+   analogWrite(9, currentSpeed);
+   analogWrite(5, currentSpeed);
+     } 
+  
+
+
+  //Forward
+  if(string =="F") { 
+  digitalWrite(6,HIGH);
+   digitalWrite(10,HIGH); 
+   analogWrite(9, currentSpeed);
+   analogWrite(5, currentSpeed);
+  }
+
+//ForwardRight
+if(string =="FR") { 
+   digitalWrite(10,HIGH);
+   analogWrite(5, currentSpeed*0.4);
+    digitalWrite(6,HIGH);
+     analogWrite(9, (currentSpeed));
+  }
+
+//ForwardLeft
+if(string =="FL") { 
+   digitalWrite(6,HIGH);
+   analogWrite(9, currentSpeed*0.4);
+   analogWrite(5, (currentSpeed));
+   digitalWrite(10,HIGH);
+  }
+//BackwardLeft
+if(string =="BL") { 
+     digitalWrite(7,HIGH);
+     analogWrite(9, currentSpeed);
+     digitalWrite(8,HIGH);
+     analogWrite(5, currentSpeed*0.4);
+  }
+
+//BackwardRight
+if(string =="BR") { 
+    digitalWrite(8,HIGH);
+    analogWrite(5, currentSpeed); 
+     digitalWrite(7,HIGH);
+     analogWrite(9, currentSpeed*0.4);
+  }
+
+
+//Stop
+if(string =="N") { 
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+    digitalWrite(8,LOW);
+     digitalWrite(10,LOW);
+    
+  }
+
+if(string =="L") { 
+    digitalWrite(6,HIGH);
+   analogWrite(5, currentSpeed);
+   digitalWrite(8,HIGH);
+    analogWrite(9, currentSpeed); 
+    
+  }
+
+  if(string =="R") { 
+    digitalWrite(10,HIGH);
+   analogWrite(5, currentSpeed);
+   digitalWrite(7,HIGH);
+     analogWrite(9, currentSpeed);
+    
+  }
+
+
+
+
+
 delay(3);
 //Serial.println(currentSpeed);
 }
